5deac651bcbeaebcfd763a3b8182731971629cb3
Davit <daavit@yahoo.com>
2021-12-20 20:43:36 -0500

    home page desing done

---
+++ africanmarketplace/src/App.js
@@ -0,0 +1,15 @@
+import Home from './Home/Home';
+import './App.css';
+import SignIn from './Home/SignIn.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
